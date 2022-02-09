dda2452f1e59613ece68839de661372a4d3e7d57
ryuken-shin <99244270+ryuken-shin@users.noreply.github.com>
2022-02-09 08:34:50 +0700

    Update __init__.py

--- Yukki/__init__.py
+++ Yukki/__init__.py
@@ -86,121 +86,121 @@ async def initiate_bot():
     global ASSID4, ASSNAME4, ASSMENTION4, ASSUSERNAME4
     global ASSID5, ASSNAME5, ASSMENTION5, ASSUSERNAME5
     os.system("clear")
-        console.print("┌ [red]Booting Up The Clients...\n")
-        await app.start()
-        console.print("└ [green]Booted Bot Client")
-        console.print("\n┌ [red]Booting Up The Assistant Clients...")
-        if STRING1 != "None":
-            await ASS_CLI_1.start()
-            random_assistant.append(1)
-            console.print("├ [yellow]Booted Assistant Client")
-        if STRING2 != "None":
-            await ASS_CLI_2.start()
-            random_assistant.append(2)
-            console.print("├ [yellow]Booted Assistant Client 2")
-        if STRING3 != "None":
-            await ASS_CLI_3.start()
-            random_assistant.append(3)
-            console.print("├ [yellow]Booted Assistant Client 3")
-        if STRING4 != "None":
-            await ASS_CLI_4.start()
-            random_assistant.append(4)
-            console.print("├ [yellow]Booted Assistant Client 4")
-        if STRING5 != "None":
-            await ASS_CLI_5.start()
-            random_assistant.append(5)
-            console.print("├ [yellow]Booted Assistant Client 5")
-        console.print("└ [green]Assistant Clients Booted Successfully!")
-        if LOG_SESSION != "None":
-            console.print("\n┌ [red]Booting Logger Client")
-            await LOG_CLIENT.start()
-            console.print("└ [green]Logger Client Booted Successfully!")
-        if "raw_files" not in listdir():
-            mkdir("raw_files")
-        if "downloads" not in listdir():
-            mkdir("downloads")
-        if "cache" not in listdir():
-            mkdir("cache")
-        if "search" not in listdir():
-            mkdir("search")
-        console.print("\n┌ [red]Loading Clients Information...")
-        getme = await app.get_me()
-        BOT_ID = getme.id
-        if getme.last_name:
-            BOT_NAME = getme.first_name + " " + getme.last_name
-        else:
-            BOT_NAME = getme.first_name
-        BOT_USERNAME = getme.username
-        if STRING1 != "None":
-            getme1 = await ASS_CLI_1.get_me()
-            ASSID1 = getme1.id
-            ASSIDS.append(ASSID1)
-            ASSNAME1 = (
-                f"{getme1.first_name} {getme1.last_name}"
-                if getme1.last_name
+    console.print("┌ [red]Booting Up The Clients...\n")
+    await app.start()
+    console.print("└ [green]Booted Bot Client")
+    console.print("\n┌ [red]Booting Up The Assistant Clients...")
+    if STRING1 != "None":
+        await ASS_CLI_1.start()
+        random_assistant.append(1)
+        console.print("├ [yellow]Booted Assistant Client")
+    if STRING2 != "None":
+        await ASS_CLI_2.start()
+        random_assistant.append(2)
+        console.print("├ [yellow]Booted Assistant Client 2")
+    if STRING3 != "None":
+        await ASS_CLI_3.start()
+        random_assistant.append(3)
+        console.print("├ [yellow]Booted Assistant Client 3")
+    if STRING4 != "None":
+        await ASS_CLI_4.start()
+        random_assistant.append(4)
+        console.print("├ [yellow]Booted Assistant Client 4")
+    if STRING5 != "None":
+        await ASS_CLI_5.start()
+        random_assistant.append(5)
+        console.print("├ [yellow]Booted Assistant Client 5")
+    console.print("└ [green]Assistant Clients Booted Successfully!")
+    if LOG_SESSION != "None":
+        console.print("\n┌ [red]Booting Logger Client")
+        await LOG_CLIENT.start()
+        console.print("└ [green]Logger Client Booted Successfully!")
+    if "raw_files" not in listdir():
+        mkdir("raw_files")
+    if "downloads" not in listdir():
+        mkdir("downloads")
+    if "cache" not in listdir():
+        mkdir("cache")
+    if "search" not in listdir():
+        mkdir("search")
+    console.print("\n┌ [red]Loading Clients Information...")
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+    BOT_USERNAME = getme.username
+    if STRING1 != "None":
+        getme1 = await ASS_CLI_1.get_me()
+        ASSID1 = getme1.id
+        ASSIDS.append(ASSID1)
+        ASSNAME1 = (
+            f"{getme1.first_name} {getme1.last_name}"
+            if getme1.last_name
                 else getme1.first_name
+        )
+        ASSUSERNAME1 = getme1.username
+        ASSMENTION1 = getme1.mention
+    if STRING2 != "None":
+        getme2 = await ASS_CLI_2.get_me()
+        ASSID2 = getme2.id
+        ASSIDS.append(ASSID2)
+        ASSNAME2 = (
+            f"{getme2.first_name} {getme2.last_name}"
+            if getme2.last_name
+            else getme2.first_name
+        )
+        ASSUSERNAME2 = getme2.username
+        ASSMENTION2 = getme2.mention
+    if STRING3 != "None":
+        getme3 = await ASS_CLI_3.get_me()
+        ASSID3 = getme3.id
+        ASSIDS.append(ASSID3)
+        ASSNAME3 = (
+            f"{getme3.first_name} {getme3.last_name}"
+            if getme3.last_name
+            else getme3.first_name
+        )
+        ASSUSERNAME3 = getme3.username
+        ASSMENTION3 = getme3.mention
+    if STRING4 != "None":
+        getme4 = await ASS_CLI_4.get_me()
+        ASSID4 = getme4.id
+        ASSIDS.append(ASSID4)
+        ASSNAME4 = (
+            f"{getme4.first_name} {getme4.last_name}"
+            if getme4.last_name
+            else getme4.first_name
+        )
+        ASSUSERNAME4 = getme4.username
+        ASSMENTION4 = getme4.mention
+    if STRING5 != "None":
+        getme5 = await ASS_CLI_5.get_me()
+        ASSID5 = getme5.id
+        ASSIDS.append(ASSID5)
+        ASSNAME5 = (
+            f"{getme5.first_name} {getme5.last_name}"
+            if getme5.last_name
+            else getme5.first_name
+        )
+        ASSUSERNAME5 = getme5.username
+        ASSMENTION5 = getme5.mention
+    console.print("└ [green]Loaded Clients Information!")
+    console.print("\n┌ [red]Loading Sudo Users...")
+    sudoersdb = db.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    for user_id in SUDOERS:
+        if user_id not in sudoers:
+            sudoers.append(user_id)
+            await sudoersdb.update_one(
+                {"sudo": "sudo"},
+                {"$set": {"sudoers": sudoers}},
+                upsert=True,
             )
-            ASSUSERNAME1 = getme1.username
-            ASSMENTION1 = getme1.mention
-        if STRING2 != "None":
-            getme2 = await ASS_CLI_2.get_me()
-            ASSID2 = getme2.id
-            ASSIDS.append(ASSID2)
-            ASSNAME2 = (
-                f"{getme2.first_name} {getme2.last_name}"
-                if getme2.last_name
-                else getme2.first_name
-            )
-            ASSUSERNAME2 = getme2.username
-            ASSMENTION2 = getme2.mention
-        if STRING3 != "None":
-            getme3 = await ASS_CLI_3.get_me()
-            ASSID3 = getme3.id
-            ASSIDS.append(ASSID3)
-            ASSNAME3 = (
-                f"{getme3.first_name} {getme3.last_name}"
-                if getme3.last_name
-                else getme3.first_name
-            )
-            ASSUSERNAME3 = getme3.username
-            ASSMENTION3 = getme3.mention
-        if STRING4 != "None":
-            getme4 = await ASS_CLI_4.get_me()
-            ASSID4 = getme4.id
-            ASSIDS.append(ASSID4)
-            ASSNAME4 = (
-                f"{getme4.first_name} {getme4.last_name}"
-                if getme4.last_name
-                else getme4.first_name
-            )
-            ASSUSERNAME4 = getme4.username
-            ASSMENTION4 = getme4.mention
-        if STRING5 != "None":
-            getme5 = await ASS_CLI_5.get_me()
-            ASSID5 = getme5.id
-            ASSIDS.append(ASSID5)
-            ASSNAME5 = (
-                f"{getme5.first_name} {getme5.last_name}"
-                if getme5.last_name
-                else getme5.first_name
-            )
-            ASSUSERNAME5 = getme5.username
-            ASSMENTION5 = getme5.mention
-        console.print("└ [green]Loaded Clients Information!")
-        console.print("\n┌ [red]Loading Sudo Users...")
-        sudoersdb = db.sudoers
-        sudoers = await sudoersdb.find_one({"sudo": "sudo"})
-        sudoers = [] if not sudoers else sudoers["sudoers"]
-        for user_id in SUDOERS:
-            if user_id not in sudoers:
-                sudoers.append(user_id)
-                await sudoersdb.update_one(
-                    {"sudo": "sudo"},
-                    {"$set": {"sudoers": sudoers}},
-                    upsert=True,
-                )
-        SUDOERS = (SUDOERS + sudoers + OWNER_ID) if sudoers else SUDOERS
-        console.print("└ [green]Loaded Sudo Users Successfully!\n")
+    SUDOERS = (SUDOERS + sudoers + OWNER_ID) if sudoers else SUDOERS
+    console.print("└ [green]Loaded Sudo Users Successfully!\n")
 
 loop.run_until_complete(initiate_bot())
 
